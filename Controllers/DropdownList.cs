9c4e8700058410194936213ffe9ea5cd2a0390c3
Raju Ahmed <raju.ahmed@datasoft-bd.com>
2022-02-14 16:28:36 +0600

    Add project files.

---
+++ Controllers/DropdownList.cs
@@ -0,0 +1,8 @@
+﻿namespace ExampleOfSelect3.Controllers
+{
+    public class DropdownList
+    {
+        public int id { get; set; }
+        public string text { get; set; }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExampleOfSelect3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var list = new List<DropdownList>();
+
+            for (int i = 0; i < 500000; i++)
+            {
+                list.Add(new DropdownList() { id = i, text = "Test Data-" + i });
+            }
+            ViewBag.Data = JsonConvert.SerializeObject(list);
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
